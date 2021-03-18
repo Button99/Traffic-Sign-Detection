4cbfe0cad94b562e4a69932ae1693cdc8b93cc0b
Button99 <christoskoubis@yahoo.com>
2021-03-18 18:44:56 +0200

    Errors Fixedv2

--- gui.py
+++ gui.py
@@ -3,9 +3,10 @@ from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLineEdit, QMess
 from PyQt5.QtCore import pyqtSlot
 from PIL import Image
 import numpy as np
-import csv
+import tensorflow
+from keras.models import load_model
 
-model= ("PATH")
+model= load_model("trafficModel.h5")
 
 # Creating dictionary
 '''
@@ -16,7 +17,49 @@ reader= csv.reader(open("LABELS PATH"))
 for row in reader:
     dict[row[0]]= row[1:]
 '''
-
+classes = { 1:'Speed limit (20km/h)',
+            2:'Speed limit (30km/h)',
+            3:'Speed limit (50km/h)',
+            4:'Speed limit (60km/h)',
+            5:'Speed limit (70km/h)',
+            6:'Speed limit (80km/h)',
+            7:'End of speed limit (80km/h)',
+            8:'Speed limit (100km/h)',
+            9:'Speed limit (120km/h)',
+            10:'No passing',
+            11:'No passing veh over 3.5 tons',
+            12:'Right-of-way at intersection',
+            13:'Priority road',
+            14:'Yield',
+            15:'Stop',
+            16:'No vehicles',
+            17:'Veh > 3.5 tons prohibited',
+            18:'No entry',
+            19:'General caution',
+            20:'Dangerous curve left',
+            21:'Dangerous curve right',
+            22:'Double curve',
+            23:'Bumpy road',
+            24:'Slippery road',
+            25:'Road narrows on the right',
+            26:'Road work',
+            27:'Traffic signals',
+            28:'Pedestrians',
+            29:'Children crossing',
+            30:'Bicycles crossing',
+            31:'Beware of ice/snow',
+            32:'Wild animals crossing',
+            33:'End speed + passing limits',
+            34:'Turn right ahead',
+            35:'Turn left ahead',
+            36:'Ahead only',
+            37:'Go straight or right',
+            38:'Go straight or left',
+            39:'Keep right',
+            40:'Keep left',
+            41:'Roundabout mandatory',
+            42:'End of no passing',
+            43:'End no passing veh > 3.5 tons' }
 # GUI
 
 class App(QWidget):
@@ -57,14 +100,12 @@ class App(QWidget):
         print("Search button clicked")
         imgPath= self.textbox.text()
         image= Image.open(imgPath)
-        image= image.resize(32, 32)
-        image= np.expand_dims(image, axis=0)    
+        image= image.resize((32, 32))
+        image= np.expand_dims(image, axis=0)
         image= np.array(image)
         pred= model.predict_classes([image])[0]
-        print(pred)
-
-
-
+        sign = classes[pred + 1]
+        print(sign)
 
 
 if __name__ == "__main__":
